c99726437383cb1df3b1fbc66acfe2e0207897c9
Opara <oparavivian720@gmail.com>
2023-08-12 23:58:37 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Descriptiom - prints lowercase alphabet in reverse
+  * Return: Always (0)
+  */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+
+	putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
