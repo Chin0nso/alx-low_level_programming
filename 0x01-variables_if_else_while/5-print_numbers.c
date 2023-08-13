b7dc15ddc2a52a902f647ab152651d49d792ab57
Opara <oparavivian720@gmail.com>
2023-08-12 19:04:24 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main-Entry point
+  *Description-single digits number of base 10
+  *Return: Always (0)
+  */
+int main(void)
+{
+int i;
+	for (i = 0; i < 10; i++)
+
+	printf("%d\n", i);
+
+	return (0);
+}
