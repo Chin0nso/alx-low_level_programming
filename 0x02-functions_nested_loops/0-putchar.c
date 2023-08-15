bd713d1474e03431dceb754960b832dfcbafeba9
Opara <oparavivian720@gmail.com>
2023-08-15 12:31:04 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Author - Opara Vivian
+  *Description - Prints putchar
+  * Return: Always (0)
+  */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
