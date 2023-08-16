9cedf9675e5ded9fb0a28c99424ccc63a05ad7a5
Opara <oparavivian720@gmail.com>
2023-08-15 22:50:16 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+int _putchar(int c) {
+	return putchar(c);
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int c);
+
+
+
+
+
+
+
+
+
+#endif
