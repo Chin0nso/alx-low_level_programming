3c532fd4a3506cd0150014a1548f2687b7f1f762
Opara <oparavivian720@gmail.com>
2023-08-19 09:29:33 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *more_numbers - prints 10 times the numbers, from 0 to 14.
+  *
+  */
+void more_numbers(void)
+{
+	int n, i;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+
+}
