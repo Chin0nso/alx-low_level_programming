e848e7562f5c115d2972754e1c31227f001845cd
Opara <oparavivian720@gmail.com>
2023-08-19 09:12:26 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,12 @@
 /**
   *print_most_numbers - prints numbers from 0 to 9 except 2 and 4
   *
-  *Return: (0)
   */
 void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		if (n != 2 && n != 4)
 		_putchar((n % 10) + '0');
