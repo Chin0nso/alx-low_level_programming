0d29f710bbc3ce87b5ee5c0c154bf6b6fd312696
Opara <oparavivian720@gmail.com>
2023-09-02 03:59:24 -0400

    prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *@argc: number of arguments
+  *@argv: array of string
+  * Return: (0)
+  */
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+
+	return (0);
+}
