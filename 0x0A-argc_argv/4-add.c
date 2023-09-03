6efe5737e66e3ed130208535a747c14e39514398
Opara <oparavivian720@gmail.com>
2023-09-03 08:11:36 -0400

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  * main - entry point
+  *@argc: number of arguments
+  *@argv: array of string
+  * Return: (0)
+  */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+			printf("Error\n");
+			return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+		printf("%d\n", sum);
+	return (0);
+}
