000d8ae699952474e805a7829b5c0f494ed67ec0
Opara <oparavivian720@gmail.com>
2023-09-15 04:58:46 -0400

    dog.h

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -2,10 +2,10 @@
 #include "dog.h"
 
 /**
-   * main - check the code
-    *
-     * Return: Always 0.
-      */
+  * main - check the code
+  *
+  * Return: Always 0.
+  */
 int main(void)
 {
 	struct dog my_dog;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
